ab70b648cb182757aebc20987af87718cd6327d2
cconstanz <co.cerdab@duocuc.cl>
2026-06-21 15:28:44 -0400

    build: refactor de configuracion, integracion de eureka y cliente feign
    - Migración a application.yml y limpieza de clases de configuración obsoletas.
    
    - Configuración de Eureka Discovery Client en main class.
    
    - Implementación de NotificacionClient para comunicación inter-servicios.
    
    - Actualización de scripts de base de datos Flyway.

--- src/main/java/com/gymapp/ms_suscripciones/MsSuscripcionesApplication.java
+++ src/main/java/com/gymapp/ms_suscripciones/MsSuscripcionesApplication.java
@@ -2,17 +2,18 @@ package com.gymapp.ms_suscripciones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
 @EnableFeignClients
-@SpringBootApplication
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class MsSuscripcionesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsSuscripcionesApplication.class, args);
 	}
 }
-
 
